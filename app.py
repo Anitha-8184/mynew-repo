305230e99898bd722495c8458b4761da809419e1
ANITHA GUNNA <108273294+Anitha-8184@users.noreply.github.com>
2025-11-24 20:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 i am writing here python content
 welcome to python code
 here we are writing content
-hello everyonr
+hello everyone
+
+here i am starting my jenkins pipeline
+now learing jekins
+started with newcourse
